e33baddd1f9548fbd9e5663e7c1a360848d9a134
GORIGORI <41143969+hita1999@users.noreply.github.com>
2021-02-24 00:29:22 +0900

    入力値の合計を出力

---
+++ sum_numbers.js
@@ -0,0 +1,19 @@
+function sumInput() {
+    let data = [];
+  
+    while (true) {
+      let input = prompt('数を入力してください');
+  
+      if ((input == '') | (input == null) | (input == false)) break;
+  
+      data.push(Number(input));
+    }
+  
+    let sum = 0;
+    for (let element of data) {
+      sum += element;
+    }
+    return sum;
+  }
+  
+  alert(sumInput());
