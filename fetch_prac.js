2768e4802b92dbe600ec05b920720d75ef8ff60c
GORIGORI <41143969+hita1999@users.noreply.github.com>
2021-03-03 14:33:42 +0900

    fetch_arrow_asyc_practice

--- fetch_prac.js
+++ fetch_prac.js
@@ -1,4 +1,15 @@
 function request() {
+    const input = '2+3*sqrt(100)';
+    const encodedInput = encodeURIComponent(input);
+    const response = fetch('http://api.mathjs.org/v4/?expr=' + encodedInput)
+        .then(function(response) {
+            return response.text()
+        }).then(function (result) {
+            console.log(result);
+        })
+}
+
+function request_arrow() {
     const input = '2+3*sqrt(4)';
     const encodedInput = encodeURIComponent(input);
     const response = fetch('http://api.mathjs.org/v4/?expr=' + encodedInput)
@@ -8,3 +19,15 @@ function request() {
             console.log(result);
         })
 }
+
+async function request_async() {
+    const input = '2+3*sqrt(4)';
+    const encodedInput = encodeURIComponent(input);
+    const response = await fetch('http://api.mathjs.org/v4/?expr=' + encodedInput);
+    const result = await response.text();
+    console.log(result);
+}
+
+request();
+request_arrow();
+request_async();
